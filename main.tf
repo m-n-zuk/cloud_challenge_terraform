22ae68c77279c2d82da7ed18426a3a56ad5874fa
m-n-zuk <magdalena.n.zuk@gmail.com>
2023-04-04 00:58:59 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,3 @@
-resource "google_compute_instance" "dareit-vm-ci" {
-  name         = "dareit-vm-tf-ci"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
-
-  tags = ["dareit"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        managed_by_terraform = "true"
-      }
-    }
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
-  }
-}
-
-# modify the code of the main.tf to add a new bucket
-
 resource "google_storage_bucket" "bucket" {
   name = "task_7_terraform_ci"
   project = "data-tangent-378423"
