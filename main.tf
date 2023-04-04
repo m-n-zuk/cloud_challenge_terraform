eeae929c3909538ba44ae24cf56d359f6a2b0702
m-n-zuk <magdalena.n.zuk@gmail.com>
2023-04-04 21:25:50 +0200

    fix: back to the original version of the code

--- main.tf
+++ main.tf
@@ -22,12 +22,3 @@ resource "google_compute_instance" "dareit-vm-ci" {
     }
   }
 }
-
-# comment
-
-resource "google_storage_bucket" "bucket" {
-  name       = "task_7_terraform_ci"
-  project       = "data-tangent-378423"
-  storage_class = "standard"
-  location      = "EU"
-}
