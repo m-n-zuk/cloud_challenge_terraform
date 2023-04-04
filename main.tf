8da23d8fa0b8527f5a6143cdcf03309418bfa82b
m-n-zuk <magdalena.n.zuk@gmail.com>
2023-04-04 20:48:51 +0200

    fix: add bucket

--- main.tf
+++ main.tf
@@ -23,11 +23,9 @@ resource "google_compute_instance" "dareit-vm-ci" {
   }
 }
 
-# modify the code of the main.tf to add a new bucket
-
 resource "google_storage_bucket" "bucket" {
-  name = "task_7_terraform_ci"
-  project = "data-tangent-378423"
+  name          = "task_7_terraform_ci"
+  project       = "data-tangent-378423"
   storage_class = "standard"
-  location = "EU"
+  location      = "EU"
 }
